e641ef89088241bf7d8f023166a36db752473c1f
starfruitone <starfruitworld@gmail.com>
2026-02-25 10:50:59 +0100

    fix: typo: incorrect brace placement

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -7,9 +7,12 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-{	char c;
+{
+	char c;
+
 	for (c = 'a'; c <= 'z'; c++)
-	{		if (c != 'e' && c != 'q')
+	{
+		if (c != 'e' && c != 'q')
 			putchar(c);
 	}
 	putchar('\n');
