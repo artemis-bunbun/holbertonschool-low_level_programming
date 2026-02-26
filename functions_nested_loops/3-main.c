54075a2b85e811ae2727f6776b1ab290aae6ae97
starfruitone <starfruitworld@gmail.com>
2026-02-26 17:01:56 +0100

    fix: error: wrong syntax?

--- functions_nested_loops/3-main.c
+++ functions_nested_loops/3-main.c
@@ -1,16 +1,19 @@
-#include "main.c"
+#include "main.h"
 /**
  * main - Entry point
  * Description: main.c file for task 3 - is lower
  * Return: Always 0 (Success)
  */
 int main(void)
-{	char c;
-	for (c = 'A'; c <= 'z'; c++)
-	{		if (_islower(c))
-		{			_putchar(c);
-		}
-	}
-	_putchar('\n');
-	return (0);
+{
+    int r;
+
+    r = _islower('H');
+    _putchar(r + '0');
+    r = _islower('o');
+    _putchar(r + '0');
+    r = _islower(108);
+    _putchar(r + '0');
+    _putchar('\n');
+    return (0);
 }
