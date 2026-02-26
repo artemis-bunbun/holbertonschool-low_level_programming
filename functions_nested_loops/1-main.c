353fd24d0ee597789385b7264f079739a8eaae5f
starfruitone <starfruitworld@gmail.com>
2026-02-26 14:56:19 +0100

    feat: creation: alphabet and main.c file

---
+++ functions_nested_loops/1-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * 
+ * Description: main.c file for task 1
+ * 
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{	print_alphabet();
+	return (0);
+}
