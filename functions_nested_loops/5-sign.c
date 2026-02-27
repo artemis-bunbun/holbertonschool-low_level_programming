49483daf3908da302e6cb55c4d0d0240d60b51bd
starfruitone <starfruitworld@gmail.com>
2026-02-27 14:32:39 +0100

    fix: syntax: written the program 5-sign.c

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - entry point
+ * description: program 5-sign.c for task 5 - print_sign
+ * Return: Always 0 (Success)
+ */
+int print_sign(int n)
+{	if (n > 0)
+	{		return (1);
+	}
+	else if (n == 0)
+	{		return (0);
+	}
+	else	{		return (-1);
+	}
+}
