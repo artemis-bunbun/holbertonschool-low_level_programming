4677ec37dcdbcfbce3aff09c304e4b1eb1df8210
starfruitone <starfruitworld@gmail.com>
2026-02-27 09:10:16 +0100

    feat: creation: 4-main.c file for task 4

--- functions_nested_loops/4-main.c
+++ functions_nested_loops/4-main.c
@@ -1,22 +1,18 @@
 #include "main.h"
 /**
  * main - entry point
- * Description: isalpha.c file for task 4
+ * Description: main.c file for isalpha.c file for task 4
  * Return: Always 0 (Success)
  */
 int main(void)
 {	char c;
 	for (c = 'A'; c <= 'z'; c++)
-	{
-		if (_isalpha(c))
-		{
-			_putchar('1');
+	{		if (_isalpha(c))
+		{			_putchar('1');
 		}
 		else
-		{
-			_putchar('0');
+		{			_putchar('0');
 		}
-	}
-	_putchar('\n');
+	}	_putchar('\n');
 	return (0);
 }
