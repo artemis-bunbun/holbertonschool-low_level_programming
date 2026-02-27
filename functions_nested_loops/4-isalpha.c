c7c40b39d637613730c8af63621df2a1068a4ab8
starfruitone <starfruitworld@gmail.com>
2026-02-27 09:03:29 +0100

    feat: creation: task 4 isalpha program

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * main - entry point
+ * Description: isalpha.c file for task 4
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{	char c;
+	for (c = 'A'; c <= 'z'; c++)
+	{
+		if (_isalpha(c))
+		{
+			_putchar('1');
+		}
+		else
+		{
+			_putchar('0');
+		}
+	}
+	_putchar('\n');
+	return (0);
+}
