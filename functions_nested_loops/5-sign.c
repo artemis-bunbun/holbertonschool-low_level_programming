14c8807b6adb38f16afcb96f1da6aa6cf727a732
starfruitone <starfruitworld@gmail.com>
2026-02-27 15:19:44 +0100

    fix: typo: wrong code

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -1,16 +1,24 @@
 #include "main.h"
 /**
- * main - entry point
- * description: program 5-sign.c for task 5 - print_sign
- * Return: Always 0 (Success)
+ * print_sign - prints the sign of a number
+ * @n: the number to check
+ * Return: 1 if positive, 0 if zero, -1 if negative
  */
 int print_sign(int n)
-{	if (n > 0)
-	{		return (1);
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
 	}
 	else if (n == 0)
-	{		return (0);
+	{
+		_putchar('0');
+		return (0);
 	}
-	else	{		return (-1);
+	else
+	{
+		_putchar('-');
+		return (-1);
 	}
 }
