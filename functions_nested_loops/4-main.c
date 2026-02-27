e021c11b017e6ef7b1020c083dc0ddc3d1b98763
starfruitone <starfruitworld@gmail.com>
2026-02-27 09:12:51 +0100

    fix: error: wrong main.c syntax

--- functions_nested_loops/4-main.c
+++ functions_nested_loops/4-main.c
@@ -5,14 +5,17 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-{	char c;
-	for (c = 'A'; c <= 'z'; c++)
-	{		if (_isalpha(c))
-		{			_putchar('1');
-		}
-		else
-		{			_putchar('0');
-		}
-	}	_putchar('\n');
+{
+	int r;
+
+	r = _isalpha('H');
+	_putchar(r + '0');
+	r = _isalpha('o');
+	_putchar(r + '0');
+	r = _isalpha(108);
+	_putchar(r + '0');
+	r = _isalpha(';');
+	_putchar(r + '0');
+	_putchar('\n');
 	return (0);
 }
