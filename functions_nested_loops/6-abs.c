a8c2ae841bd20aac462e48e81a15ed726d7fca95
starfruitone <starfruitworld@gmail.com>
2026-02-27 15:26:40 +0100

    feat: creation: syntax/code for 6-abs.c

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * Description: program for task 6 - absolute value
+ * Return: Always 0 (Success)
+ */
+
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (-n);
+	}
+	else
+	{
+		return (n);
+	}
+}
