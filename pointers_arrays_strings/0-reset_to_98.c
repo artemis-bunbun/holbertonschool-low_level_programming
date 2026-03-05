28fffd2cf82239ae10b05708606bec1afc770226
artemisbunbun <starfruitworld@gmail.com>
2026-03-05 12:49:13 +0000

    fix: syntax: errors

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,11 +1,10 @@
-#include "main.c"
 #include "main.h"
 #include <stdio.h>
 
 /**
  * main - check the code 
  * description: script that takes a pointer and updates the value it points to 98
- * Return: Always 0.
+ * @n: pointer to an int
  */
 
  void reset_to_98(int *n)
