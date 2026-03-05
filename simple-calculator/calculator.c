6b44ea4455b2f62a482f41716884aff834e0c8f6
artemisbunbun <starfruitworld@gmail.com>
2026-03-05 10:40:07 +0000

    fix: syntax: added task 0 and task 1 choice validation

--- simple-calculator/calculator.c
+++ simple-calculator/calculator.c
@@ -2,11 +2,18 @@
 
 int main(void)
 {
+    int choice = 0;
+
     printf("Simple Calculator\n");
+    printf("\n");
     printf("1) Add\n");
     printf("2) Subtract\n");
     printf("3) Multiply\n");
     printf("4) Divide\n");
     printf("0) Quit\n");
-    return(0);
+    scanf("%d", &choice);
+    printf("\n");
+    printf("Choice: %d", choice);
+    printf("\n");
+    return (0);
 }
