083ef5d9d47ab89e68bc1273d12e236604436eb5
artemisbunbun <starfruitworld@gmail.com>
2026-03-05 13:21:37 +0000

    fix: syntax: wrote the swapping function

--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -2,7 +2,14 @@
 
 /**
  * Description: this script swaps the values to two ints
+ * @a: pointer to int 1
+ * @b: poniter to int 2
  */
 
-void swap_int(int *a, int *b);
-
+void swap_int(int *a, int *b)
+{
+	int temp;
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
