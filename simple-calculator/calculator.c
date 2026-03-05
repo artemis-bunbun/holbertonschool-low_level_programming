84e2845f9048fe04c915c0f594b81e7252cfc96c
artemisbunbun <starfruitworld@gmail.com>
2026-03-05 12:13:34 +0000

    fix: syntax: task 2 sum of a and b added

--- simple-calculator/calculator.c
+++ simple-calculator/calculator.c
@@ -3,23 +3,42 @@
 int main(void)
 {
     int choice = 0;
-
+    int calc = 1;
+    int a;
+    int b;
+    int sum;
     printf("Simple Calculator\n");
     printf("\n");
-    printf("1) Add\n");
-    printf("2) Subtract\n");
-    printf("3) Multiply\n");
-    printf("4) Divide\n");
-    printf("0) Quit\n");
-    scanf("%d", &choice);
-    printf("\n");
-    printf("Choice: %d", choice);
-    printf("\n");
-
-    if (choice == 0)
+    while (calc == 1)
     {
-        printf("Bye!\n");
+        printf("Choice: %d\n", choice);
+        printf("1) Add\n");
+        printf("2) Subtract\n");
+        printf("3) Multiply\n");
+        printf("4) Divide\n");
+        printf("0) Quit\n");
+
+        scanf("%d", &choice);
+        if (choice > 4)
+        {
+            printf("Invalid choice\n");
+        }
+        if (choice == 0)
+        {
+            printf("Bye!\n");
+            calc = 0;
+        }
+        if (choice == 1)
+        {
+            printf("enter number for A:\n");
+            scanf("%d", &a);
+            printf("enter number for B:\n");
+            scanf("%d", &b);
+            sum = a + b;
+            printf("Result: %d\n", sum);
+            calc = 0;
+        }
+        
     }
-    
     return (0);
 }
