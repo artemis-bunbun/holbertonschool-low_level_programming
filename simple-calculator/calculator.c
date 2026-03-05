5b102622a5cbdc2e41a92de9bd41969a8b103d68
artemisbunbun <starfruitworld@gmail.com>
2026-03-05 10:08:38 +0000

    feat: creation: README.md and the simple calculator

---
+++ simple-calculator/calculator.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("Simple Calculator\n");
+    printf("1) Add\n");
+    printf("2) Subtract\n");
+    printf("3) Multiply\n");
+    printf("4) Divide\n");
+    printf("0) Quit\n");
+    return(0);
+}
