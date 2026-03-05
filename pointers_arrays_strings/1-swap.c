ea0453cc7c7845dcc0ec6e47ba15b583176de720
artemisbunbun <starfruitworld@gmail.com>
2026-03-05 13:17:21 +0000

    fix: syntax: code implemented

--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -1,5 +1,8 @@
 #include "main.h"
 
 /**
- * Description: this script swaps the values to two ints 
+ * Description: this script swaps the values to two ints
  */
+
+void swap_int(int *a, int *b);
+
