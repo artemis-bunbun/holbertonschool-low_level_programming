2fa7424199e0a96e57d3d5cb3d56542c52c93f7e
artemisbunbun <starfruitworld@gmail.com>
2026-03-08 16:17:08 +0000

    feat: creation: task 6 chessboard print

---
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_chessboard - prints the chessboard
+ * @a: pointer to 8x8 chessboard array
+ *
+ * Return: void
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i = 0;
+	int j;
+
+	while (i < 8)
+	{
+		j = 0;
+		while (j < 8)
+		{
+			_putchar(a[i][j]);
+			j++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
