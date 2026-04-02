0683141ba58f97098024242f00e7df445ca90f1a
artemisbunbun <starfruitworld@gmail.com>
2026-04-02 08:49:58 +0000

    feat: creation: array 1 task 1 for holberton school

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,17 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - executes a function on each element of an array
+ * @array: array to iterate over
+ * @size: number of elements in the array
+ * @action: function to apply to each element
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	if (array == NULL || action == NULL)
+		return;
+	for (i = 0; i < size; i++)
+		action(array[i]);
+}
