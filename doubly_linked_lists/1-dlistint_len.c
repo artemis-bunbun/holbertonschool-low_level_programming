71c116f10cb6291b0771bd59b5809cb50e849cec
artemisbunbun <starfruitworld@gmail.com>
2026-04-03 12:05:39 +0000

    feat: creation: created the dlistint len code for task 1

---
+++ doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - returns number of elements in a dlistint_t list
+ * @h: head of the list
+ *
+ * Return: number of nodes
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t count;
+
+	count = 0;
+	while (h)
+	{
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
